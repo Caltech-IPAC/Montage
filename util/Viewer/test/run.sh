42ddb8b033d0845499b7ca8e2e1d20a31e0d56e6
JohnGood <jcg@ipac.caltech.edu>
2015-09-27 12:55:23 -0700

    Final 4.0 updates.
    Former-commit-id: 16273dbf9564ef15cc72930019483f7538c028ff

--- util/Viewer/test/run.sh
+++ util/Viewer/test/run.sh
@@ -1,38 +1,9 @@
 #!/bin/sh
 
-../mViewer  \
-   -ct       1 \
-   -csys     eq j2000 \
-   -color    808080 \
-   -grid     equ J2000 \
-   -color    FF8080 \
-   -symbol   25p compass \
-   -mark       50p  100p \
-   -symbol   50p compass \
-   -mark       50p -100p \
-   -symbol   75p compass \
-   -mark      -50p -100p \
-   -symbol   100p compass \
-   -mark      -50p  100p \
-   -gray     iras12.fits -2s max gaussian-log \
-   -png      iras12_eq.png
-
-../mViewer  \
-   -ct       1 \
-   -csys     eq j2000 \
-   -color    808080 \
-   -grid     equ J2000 \
-   -color    FF8080 \
-   -symbol   25p compass \
-   -mark       50p  100p \
-   -symbol   50p compass \
-   -mark       50p -100p \
-   -symbol   75p compass \
-   -mark      -50p -100p \
-   -symbol   100p compass \
-   -mark      -50p  100p \
-   -gray     iras12small.fits -2s max gaussian-log \
-   -png      iras12small_eq.png
-
-cp iras12_eq.png /work/jcg_9021
-cp iras12small_eq.png /work/jcg_9021
+mViewer \
+        -color  yellow  \
+        -grid   Equatorial J2000 \
+        -symbol 13p triangle  \
+        -mark   214.5666 25.2 \
+        -gray   issa.fits 0s max gaussian-log \
+        -out    issa.png
