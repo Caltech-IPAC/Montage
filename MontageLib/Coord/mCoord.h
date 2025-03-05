5cc1cea29a3f312140b1f96d9421d15a803381a6
JohnGood <jcg@ipac.caltech.edu>
2025-03-05 13:58:55 -0800

    Forgot an include file.

---
+++ MontageLib/Coord/mCoord.h
@@ -0,0 +1,10 @@
+#ifndef MCOORD_H
+#define MCOORD_H
+
+/*************************************/
+/* Define mCoord function prototypes */
+/*************************************/
+
+int mCoord_getPlanes(char *, int *);
+
+#endif
