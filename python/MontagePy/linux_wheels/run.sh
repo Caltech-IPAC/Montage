ea8312abcd0dd819817a88d52f7f509a29658b14
JohnGood <jcg@ipac.caltech.edu>
2025-06-05 18:17:35 -0700

    Rearrange wheel build files.

--- python/MontagePy/linux_wheels/run.sh
+++ python/MontagePy/linux_wheels/run.sh
@@ -1,12 +1,33 @@
 #!/bin/sh
 
+# The cibuildwheel utility can handle both linux and macos builds but in different
+# ways.  For Linux it uses specially constructed Docker linux containers that have all
+# the needed Python versions installed (Docker itself must already be configured installed
+# on the system) and for MacOS it uses the local Mac compiler (which supports explicit
+# deployment targets) but the versions of Python have to be manually installed.
+#
+# For Linux, we have to be very careful to remember that everything done in the build
+# happens inside the Docker container and that no changes we make locally will have to
+# be pushed to GitHub before they will appear in the build.
+#
+# So inside the make.sh script we will install Montage from GitHub into /project inside
+# the container, then build it, arrange files from it for cibuildwheel and setup.py,
+# and run cibuildwheel all still inside the container once the container exits everything
+# except the wheel files themselves (which will have been written to a container-external
+# directory call 'wheelhouse') will go away.
+#
+# The cibuildwheel utility uses environment variables to control what it actually
+# builds.  
+
 export OS='linux'
 export CIBW_BUILD='*'
+export CIBW_SKIP='cp36-* cp37-*'
 export CIBW_ARCHS='x86_64'
 export CIBW_BEFORE_ALL='sh make.sh'
 export CIBW_BUILD_FRONTEND='build'
 
 echo "CIBW_BUILD>         " "$CIBW_BUILD"
+echo "CIBW_SKIP>          " "$CIBW_SKIP"
 echo "CIBW_ARCHS>         " "$CIBW_ARCHS"
 echo "CIBW_BEFORE_ALL>    " "$CIBW_BEFORE_ALL"
 echo "CIBW_BUILD_FRONTEND>" "$CIBW_BUILD_FRONTEND"
--- python/MontagePy/macos_wheels/run.sh
+++ python/MontagePy/macos_wheels/run.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
-# Before running this script, there are a few things that need to get
-# installed if you don't already have them.
-
-# The most important thing is to install the versions of Python we are trying to
-# support.  The varies with time as versions appear and others get sunsetted.
+# The cibuildwheel utility can handle both linux and macos builds but in different
+# ways.  For Linux it uses specially constructed Docker linux containers that have all
+# the needed Python versions installed (Docker itself must already be configured installed
+# on the system) and for MacOS it uses the local Mac compiler (which supports explicit
+# deployment targets) but the versions of Python have to be manually installed.
+#
+# The set of Pythons varies with time as versions appear and others get sunsetted.
 # It is best to install the copies from python.org (not, for instance, from Anaconda)
 # using the .pkg files the have there.  The cibuildwheel utility will find and use
 # each of these in turn (based on the CIBW_BUILD environment variable below).
 
 # These instances of python get installed in /Library/Frameworks/Python.framework/Versions
-# and don't interfer with any other copy you might have so you can delete them when you
+# and don't interfere with any other copy you might have so you can delete them when you
 # are sure you are done using them.
 
 # We might be able to use the local copy of our Montage (outside the container) as the
-# basis of the build but we instead pull a new copy into the container and compile it there. 
+# basis of the build but we instead pull a new copy from GitHub compile it locally.
 # If we used the local copy, we would need to compile it with the right OSX deployment
 # target and that could get overly confusing.
 
@@ -23,22 +25,22 @@
 # variable to determine the OS version target for the compilation (a necessary concern
 # for MacOS builds and OS versions must match if you are linking code together).
 
-# Different settings are used for Linux, so we set things depending on what 'uname'
-# says about the system.
 
 export OS='macos'
-export CIBW_BUILD='*'
+export CIBW_BUILD='cp38-*'
 export CIBW_SKIP='cp36-* cp37-*'
 export CIBW_ARCHS='x86_64 universal2 arm64'
 export CIBW_BEFORE_ALL='sh make.sh'
-export MACOSX_DEPLOYMENT_TARGET='11.1'
 export CIBW_BUILD_FRONTEND='build'
+export MACOSX_DEPLOYMENT_TARGET='11.1'
 
-echo "OS>                 " "$OS"
-echo "CIBW_ARCHS>         " "$CIBW_ARCHS"
-echo "CIBW_BUILD>         " "$CIBW_BUILD"
-echo "CIBW_BEFORE_ALL>    " "$CIBW_BEFORE_ALL"
-echo "CIBW_BUILD_FRONTEND>" "$CIBW_BUILD_FRONTEND"
+echo "OS>                      " "$OS"
+echo "CIBW_BUILD>              " "$CIBW_BUILD"
+echo "CIBW_SKIP>               " "$CIBW_SKIP"
+echo "CIBW_ARCHS>              " "$CIBW_ARCHS"
+echo "CIBW_BEFORE_ALL>         " "$CIBW_BEFORE_ALL"
+echo "CIBW_BUILD_FRONTEND>     " "$CIBW_BUILD_FRONTEND"
+echo "MACOSX_DEPLOYMENT_TARGET>" "$MACOSX_DEPLOYMENT_TARGET"
 
 
 # Build all the MacOS-related wheels
